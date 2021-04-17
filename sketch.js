a0e6e5a2de2fa71e363780f9e9a786d48d7177ff
priya-shanmus <pria.612@gmail.com>
2021-04-17 15:49:45 +0530

    added boxes

--- sketch.js
+++ sketch.js
@@ -22,6 +22,10 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
